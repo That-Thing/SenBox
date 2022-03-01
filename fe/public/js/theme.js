b4c5c177c007d266586e5f4eda3f29bba4da9c80
Sen <sen@onee.ch>
2022-02-28 22:30:45 -0500

    Pressing the theme change button now changes the theme on the current page using the amazing power of magic (and javascript)

--- fe/public/js/theme.js
+++ fe/public/js/theme.js
@@ -3,9 +3,18 @@ function setTheme(theme) {
     if(theme == "dark") {
         $("#theme-button").attr('onClick', 'setTheme("light")');
         $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
-    } else {
+        $('.bg-light').removeClass('bg-light').addClass('bg-dark');
+        $('.navbar-light').removeClass('navbar-light').addClass('navbar-dark');
+        $('.theme-light').removeClass('theme-light').addClass('theme-dark');
+    } else if(theme == "light") {
         $("#theme-button").attr('onClick', 'setTheme("dark")');
         $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
-    } 
+        $('.bg-dark').removeClass('bg-dark').addClass('bg-light');
+        $('.navbar-dark').removeClass('navbar-dark').addClass('navbar-light');
+        $('.theme-dark').removeClass('theme-dark').addClass('theme-light');
+
+    } else {
+        console.log("What do you think you're doing?");
+    }
     
 };
